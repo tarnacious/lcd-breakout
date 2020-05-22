6ed2a2b269f27a9beae2a27d47db3f336bea0f81
tarnacious <tarn@tarnbarford.net>
2020-05-22 15:11:01 +0000

    show a countdown on a display

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
 #include <SevenSegment.h>
+#include <stdio.h>
 
 #define CLOCK 3 // Arudino digital 44 -> AY0438 clock
 #define DATA 4	 // Arudino digital 46 -> AY0438 data
@@ -10,59 +11,60 @@
 
 SevenSegment screen(CLOCK, DATA, LOAD);
 
-int period = 1000;
-unsigned long time_now = 0;
+void print_millis(unsigned long milli)
+{
+  //3600000 milliseconds in an hour
+  unsigned long hr = milli / 3600000;
+  milli = milli - 3600000 * hr;
+  //60000 milliseconds in a minute
+  unsigned long min = milli / 60000;
+  milli = milli - 60000 * min;
+  //1000 milliseconds in a second
+  unsigned long sec = milli / 1000;
 
-int led = 13;
+  char result[6];
+  sprintf(result, "%02d:%02d", int(min), int(sec));
+  screen.print(result);
+}
 
 void run_lcd(void)
 {
+  unsigned long duration = 5000; //1800000;
+
   Serial.print("Setup\n");
-	screen.begin("AY0438","8");
+	screen.begin("AY0438","8.8|8.8");
+	char message[] = "yayy";
+  screen.print(message);
+  delay(1000);
+  screen.clear();
+  delay(1000);
 
-	// Turn on all segments and turn off 5 times
-	for (int i=0; i<5; i ++){
-    Serial.print("Turn all segments on\n");
-		// Turn all known segments on
-		screen.on();
-		delay(500);
+  print_millis(duration);
+  delay(1000);
+  unsigned long start_millis = millis();
 
-		// Turn all known segments off
-    Serial.print("Turn all segments off\n");
-		screen.clear();
-		delay(500);
-	}
-
-  Serial.print("Count to 9");
-	// Count to 9
-	for (int i = 0; i < 10; i++){
-		screen.printNumber(i);
-		delay(200);
-	}
-
-	// Display all characters one by one
-  Serial.print("all characters");
-	screen.setAlignment(LEFT);
-	char c[2];
-	for (int i = 32; i<= 127; i++){
-		c[0] = i;
-		c[1] = 0;
-		screen.print(c);
-		delay(200);
-	}
+	for (;;) {
+    unsigned long current_millis = millis();
+    unsigned long elapsed_millis = current_millis - start_millis;
+    if (elapsed_millis > duration) {
+      for(int i = 0; i < 8; i++) {
+        print_millis((unsigned long)0);
+        delay(400);
+        screen.clear();
+        delay(400);
+      }
+      delay(2000);
+      print_millis(duration);
+      delay(1000);
+      start_millis = millis();
+    } else {
+      print_millis(duration - elapsed_millis);
+    }
+    delay(1);
+  }
+	screen.clear();
 }
 
-void flash_led(void)
-{
-	pinMode(led, OUTPUT);
-	while(true){
-		Serial.print("flash\n");
-		digitalWrite(led, LOW);
-		delay(500);
-		digitalWrite(led, HIGH);
-		delay(500);
-	}
-}
 
 int main(void)
 {
